73c51468a440a19618c6d55b19e839c2b21216aa
ZhaoXin <iamzax@163.com>
2024-06-28 22:47:36 +0800

    init

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,34 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
+import viteCompression from 'vite-plugin-compression'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
+const pathResolve = (dir: string) => {
+    return resolve(__dirname, '.', dir)
+}
+
+const alias: Record<string, string> = {
+    '/@': pathResolve('./src/'),
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: '../../build/web' // 将输出目录设置为 custom-dist
-  }
+    plugins: [
+        vue(),
+        viteCompression(),
+        VueSetupExtend()
+    ],
+    base: './',
+    resolve: {alias},
+    build: {
+        outDir: '../../build/web', // 将输出目录设置为 custom-dist
+        // 清除console等多余代码
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true
+            }
+        },
+    }
 })
